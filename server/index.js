8eef1cc123a8acb1ffa87f91c909fcdc1c73b1a4
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-06-14 12:29:00 +0100

    Working routing

--- server/index.js
+++ server/index.js
@@ -5,17 +5,16 @@ const path = require('path')
 
 const app = express()
 
+const Routes = require('./routes')
+app.use('/', Routes)
+
 const options = {
   key: fs.readFileSync(path.resolve(__dirname, './certs/server.key')),
   cert: fs.readFileSync(path.resolve(__dirname, './certs/server.cert'))
 }
 
-app.get('/', (req, res) => {
-  res.send('Hi there')
-})
-
-// app.listen(3000, () => {
-//   console.log('Server running')
+// app.get('/', (req, res) => {
+//   res.send('Hi there')
 // })
 
 https.createServer(options, app).listen(3000)
