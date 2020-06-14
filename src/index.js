a22966f71b23a65b023f385576fe901af9b3b314
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-06-14 17:10:42 +0100

    Setting up React working environment

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import TextArea from './components/text-area'
-import EditingArea from './components/editing-area'
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      data: 'Lorem ipsum dolor sit amet',
-      editing: false
-    }
-  }
+const App = () => {
+  const [auth, setAuth] = useState()
 
-  handleClick () {
-    console.log('Test')
-    this.setState({ editing: !this.state.editing })
-  }
-
-  render () {
-    return (
-      <div className="app">
-        {
-          this.state.editing ?
-            <EditingArea
-              return={() => this.handleClick()}
-            /> :
-            <TextArea
-              data={this.state.data}
-              onClick={() => this.handleClick()}
-            />
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="app">
+      <input type="text" placeholder="user name" />
+      <input type="text" placeholder="password" />
+      <button onClick={() => setAuth('test')}>Login</button>
+      <h2>{auth}</h2>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
